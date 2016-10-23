7533543c8a9a4aa4b55d833aedfddeea6f5a2ce0
6148nathalie <tsanceo@yahoo.com>
2016-10-23 16:09:30 +0200

    add db and call db

--- web/index.php
+++ web/index.php
@@ -1,11 +1,28 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link href="microcms.css" rel="stylesheet" />
+    <title>infolingo</title>
+</head>
+<body>
+<header>
+    <h1>infolingo</h1>
+</header>
+<form action="action.php" method="post">
+    <p>
+        Traduction :
+        <input type="text" name="traduction" />
+        <input type="submit" name="valider" />
+    </p>
+</form>
 <?php
-
-require_once __DIR__.'/../vendor/autoload.php';
-
-$app = new Silex\Application();
-
-$app->get('/', function () {
-    return 'Hello world';
-});
-
-$app->run();
+$bdd = new PDO('mysql:host=localhost;dbname=infolingo;charset=utf8', 'root', '');
+$words = $bdd->query('SELECT * FROM word ORDER BY RAND()');
+foreach ($words as $word): ?>
+    <article>
+        <h3><?php echo $word['word'] ?></h3>
+    </article>
+<?php endforeach ?>
+</body>
+</html>
